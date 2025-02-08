c817f6bd3f6944fffec022c8782d104e6137cf48
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-08 16:24:48 +0530

    add routing for HomePage, About, Dashboard, and Login components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Button variant ="destructive" >
-      Hello Big B</Button></>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
