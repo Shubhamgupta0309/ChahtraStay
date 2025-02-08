0ef41f55cf98ba849c55435e3e0f1231dc246c38
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-08 23:40:17 +0530

    set up server with database connection and API routes for authentication and hostel management

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,21 @@
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import hostelRoutes from "./routes/hostelRoutes.js";
 dotenv.config();
 const app = express();
-
+app.use(express.urlencoded());
 const PORT = process.env.PORT;
+
+connectDB();
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    message: "Welcom to chahtraStay",
+  });
+});
+app.use("/api/auth", userRoutes);
+app.use("/api/hostel", hostelRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
