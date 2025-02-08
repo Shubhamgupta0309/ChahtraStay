39356f3ab90fa595f8a8c781b03d1628f73b9c06
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-08 18:40:25 +0530

    add Footer component for site-wide footer display

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full py-6 bg-gray-800 text-white text-center">
+      © 2025 ChahtraStay. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
