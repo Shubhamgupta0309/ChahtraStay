dcbf1e981ad9e470e7ada5cafc7984adb85fe6cd
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-09 19:01:22 +0530

    add CORS middleware to enable cross-origin requests based on environment variable

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,14 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import hostelRoutes from "./routes/hostelRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
+import cors from "cors"
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin:process.env.CORS_ORIGIN
+}))
 const PORT = process.env.PORT;
 
 connectDB();
