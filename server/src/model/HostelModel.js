e924607efd0c40c0729e75941cd6286c122d88e8
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-09 16:29:50 +0530

    enhance hostel model with unique ID field and standardize hostel type values

--- server/src/model/HostelModel.js
+++ server/src/model/HostelModel.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const hostelSchema = new mongoose.Schema({
+  hostelId:{type:String, required:true},
   name: { type: String, required: true },
   location: { type: String, required: true },
   price: { type: Number, required: true },
   amenities: { type: [String], default: [] },
   rating: { type: Number, default: 0 },
-  hostelType: { type: String, enum: ["Boys", "Girls", "Co-Ed"], required: true },
+  hostelType: { type: String, enum: ["boys", "girls", "co-ed"], required: true },
   rules: { type: [String], default: [] },
   food: { type: [String], default: [] },
   images: { type: [String], default: [] },
