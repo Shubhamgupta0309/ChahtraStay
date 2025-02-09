cb36872d29128664d161b5369b522758e823d858
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-09 19:01:07 +0530

    add ProtectedRoute middleware to manage access based on user authentication and loading state

---
+++ client/src/middlewares/protectedRoutes.jsx
@@ -0,0 +1,16 @@
+import Loading from "../pages/Loading"
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext"
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  console.log("ProtectedRoute: user =", user, "loading =", loading);
+
+  if (loading) return <Loading />; 
+
+  return user ? children : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
