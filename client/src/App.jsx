c0587f34fa0ce6bce76b65b90317489aff2b5ff0
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-09 19:00:28 +0530

    implement lazy loading for routes and add authentication context with protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,58 @@
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import HomePage from "./pages/HomePage";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import HostelPage from "./pages/HostelsPage";
-import HostelDetails from "./pages/HostelDetails";
-import HostelBooking from "./pages/BookHostel";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./middlewares/protectedRoutes";
+import Loading from "./pages/Loading"; 
 
+const HomePage = lazy(() => import("./pages/HomePage"));
+const HostelPage = lazy(() => import("./pages/HostelsPage"));
+const HostelDetails = lazy(() => import("./pages/HostelDetails"));
+const HostelBooking = lazy(() => import("./pages/BookHostel"));
+const About = lazy(() => import("./pages/About"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Login = lazy(() => import("./pages/Login"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/hostel" element={<HostelPage />} />
-        <Route path="/hostel/:id" element={<HostelDetails />} />
-        <Route path="/hostel/:id/book" element={<HostelBooking />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <AuthProvider>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/hostel" element={<HostelPage />} />
+            <Route path="/hostel/:id" element={<HostelDetails />} />
+            <Route
+              path="/hostel/:id/book"
+              element={
+                <ProtectedRoute>
+                  <HostelBooking />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </AuthProvider>
     </Router>
   );
 }
